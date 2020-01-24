38172961f0fbdc2e8d5689b8392f2d925fea4211
Maxime Daniel <root@maxux.net>
2020-01-24 10:50:32 +0100

    Remove legacy code handling flist via jumpscale

--- python/hub/flist.py
+++ python/hub/flist.py
@@ -36,9 +36,6 @@ class HubFlist:
         self.tmpdir = None
         self.flist = None
 
-    def dummy(self, dirobj=None, type=None, name=None, subobj=None, args=None, key=None):
-        pass
-
     def ensure(self, target):
         if not os.path.exists(target):
             os.mkdir(target)
@@ -54,15 +51,6 @@ class HubFlist:
             ssl=self.backopt['ssl']
         )
 
-    def pack(self, target):
-        """
-        Pack database into archive
-        """
-        with tarfile.open(target, "w:gz") as tar:
-            tar.add(self.tmpdir.name, arcname="")
-
-        return True
-
     def unpack(self, filepath, target=None):
         """
         Unpack tar archive `filepath` into `target` directory
@@ -73,183 +61,16 @@ class HubFlist:
         self.ensure(target)
 
         print("[+] upacking: %s" % filepath)
-        """
-        t = tarfile.open(filepath, "r:*")
-        t.extractall(path=target)
-
-        filescount = len(t.getnames())
-        t.close()
-        """
         args = ["tar", "-xpf", filepath, "-C", target]
         p = subprocess.Popen(args)
         p.wait()
 
         return 0
-
-    def initialize(self, rootpath, prefix="flist-"):
-        self.tmpdir = self.workspace("flist-")
-        self.flist = self.open(rootpath, self.tmpdir.name)
-
-        print("[+] flist initialized: %s, %s\n" % (rootpath, self.tmpdir.name))
-
-        return True
-
-    def open(self, rootpath, sourcepath):
-        kvs = j.data.kvs.getRocksDBStore('flist', namespace=None, dbpath=sourcepath)
-        flist = j.tools.flist.getFlist(rootpath=rootpath, kvs=kvs)
-
-        return flist
-
     def workspace(self, prefix="workspace-"):
         return tempfile.TemporaryDirectory(prefix=prefix, dir=self.config['flist-work-directory'])
 
-    def insert(self, directory, excludes=[".*\.pyc", ".*__pycache__"]):
-        self.flist.add(directory, excludes=excludes)
-
-    def commit(self):
-        """
-        This is a workaround to ensure file are written and not in a unstable state
-        this access 'protected' class member, this could be improved
-        """
-        print("[+] flist: committing (compacting db)")
-        self.flist.dirCollection._db.rocksdb.compact_range()
-
-    def upload(self):
-        print("[+] flist: populating contents")
-        self.flist.populate()
-        self.commit()
-
-        r = self.backend()
-        self.proceed = 0
-
-        def procFile(dirobj, type, name, subobj, args):
-            fullpath = "%s/%s/%s" % (self.flist.rootpath, dirobj.dbobj.location, name)
-            # print("[+] uploading: %s" % fullpath)
-
-            self.proceed += 1
-            if self.proceed % 150 == 0:
-                print("[+] still uploading [%d]" % self.proceed)
-
-            """
-            import hashlib
-            m = hashlib.md5()
-
-            with open(fullpath, "rb") as x:
-                data = x.read()
-
-            print(len(data))
-            m.update(data)
-            key = m.hexdigest()
-            print(key)
-            r.set(key, data)
-            """
-
-            hashs = g8storclient.encrypt(fullpath)
-
-            if hashs is None:
-                return
-
-            for hash in hashs:
-                if not r.exists(hash['hash']):
-                    r.set(hash['hash'], hash['data'])
-
-        print("[+] uploading contents")
-        self.process(procFile)
-
-    def process(self, callback):
-        """
-        Walk over the flist and call user-defined file callback
-        """
-        result = []
-        self.flist.walk(
-            dirFunction=self.dummy,
-            fileFunction=callback,
-            specialFunction=self.dummy,
-            linkFunction=self.dummy,
-            args=result
-        )
-
-        return result
-
-    def loads(self, source):
-        """
-        Load an existing flist into this object
-        """
-        self.tmpdir = self.workspace('flist-')
-        self.unpack(source)
-        self.flist = self.open("/", self.tmpdir.name)
-
-        return True
-
     def loadsv2(self, source):
         self.sourcev2 = source
-
-    def validate(self):
-        """
-        This validate (confirm) all contents from the flist are available on the
-        backend, this is useful when you want to ensure consistancy between one
-        flist archive and the backend (ensure all files will be found)
-
-        Returns True if backend is fully-sync, False if (at least) a single key is missing
-        """
-        r = self.backend()
-        pipe = r.pipeline()
-
-        def procFile(dirobj, type, name, subobj, args):
-            for chunk in subobj.attributes.file.blocks:
-                rkey = chunk.hash.decode('utf-8')
-                pipe.exists(rkey)
-
-        self.process(procFile)
-        result = pipe.execute()
-
-        return (not False in result)
-
-    def listing(self):
-        """
-        Walk over the full contents and returns a summary of the contents
-        """
-        contents = {
-            'content': [],
-            'regular': 0,
-            'directory': 0,
-            'symlink': 0,
-            'special': 0
-        }
-
-        def getPath(location, name):
-            if location:
-                return "/%s/%s" % (location, name)
-
-            return "/%s" % name
-
-        def procDir(dirobj, type, name, args, key):
-            contents['directory'] += 1
-            contents['content'].append({'path': getPath(dirobj.dbobj.location, name), 'size': 0})
-
-        def procSpecial(dirobj, type, name, subobj, args):
-            contents['special'] += 1
-            contents['content'].append({'path': getPath(dirobj.dbobj.location, name), 'size': 0})
-
-        def procFile(dirobj, type, name, subobj, args):
-            contents['regular'] += 1
-            contents['content'].append({'path': getPath(dirobj.dbobj.location, name), 'size': dirobj.dbobj.size})
-
-        def procLink(dirobj, type, name, subobj, args):
-            contents['symlink'] += 1
-            contents['content'].append({'path': getPath(dirobj.dbobj.location, name), 'size': 0})
-
-        print("[+] parsing database")
-        result = []
-        self.flist.walk(
-            dirFunction=procDir,
-            fileFunction=procFile,
-            specialFunction=procSpecial,
-            linkFunction=procLink,
-            args=result
-        )
-
-        return contents
 
     def listingv2(self):
         args = [self.zflist, "--list", "--action", "json", "--archive", self.sourcev2]
@@ -267,6 +88,8 @@ class HubFlist:
         p = subprocess.Popen(args, stdout=subprocess.PIPE)
         (output, err) = p.communicate()
         p.wait()
+
+        print(output)
 
         return json.loads(output.decode('utf-8'))
 
