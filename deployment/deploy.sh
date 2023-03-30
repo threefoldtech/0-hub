00a6372e69157339401011e6c918b1873f72c7a6
Maxime Daniel <root@maxux.net>
2023-03-30 15:18:41 +0200

    deploy: update 0-flist branch

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -60,7 +60,7 @@ capnp() {
 }
 
 zeroflist() {
-    git clone -b development https://github.com/threefoldtech/0-flist
+    git clone -b development-v2 https://github.com/threefoldtech/0-flist
     pushd 0-flist
 
     pushd libflist
